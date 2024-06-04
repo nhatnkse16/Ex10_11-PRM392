6e982ed9a58c1219ff6178532ffd71d89dbd60ed
nhatnkse16 <nguyenkimnhat0110@gmail.com>
2024-06-04 12:50:30 +0700

    first commit

---
+++ app/src/main/java/com/ngkn/ex10_11/PermissionsMainActivity.java
@@ -0,0 +1,50 @@
+package com.ngkn.ex10_11;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import
+        androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Toast;
+public class PermissionsMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.permissions_activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+        ), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+// Check if the permission is already granted
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+// Permission is already granted, you can perform the related task
+            Toast.makeText(this, "Permission already granted",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+// Request the permission
+            requestPermissionLauncher.launch(android.Manifest.permission.READ_CONTACTS);
+        }
+    }
+
+    // Request the permission
+    private final ActivityResultLauncher<String>
+            requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. You can perform the related task
+                    Toast.makeText(PermissionsMainActivity.this, "Permission granted", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Permission is denied. Show a message to the user
+                    Toast.makeText(PermissionsMainActivity.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            });
+}
